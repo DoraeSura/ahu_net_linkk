f46f0fdfd561465b36e36ba4ecca779914f9ac5c
DoraeSura <dorasura@126.com>
2023-07-11 15:28:34 +0800

    一建式

--- Link.py
+++ Link.py
@@ -1,99 +1,125 @@
-import socket
-import requests    # 用于向目标网站发送请求
-import pywifi
-from pywifi import const
-from subprocess import check_output
-import comtypes
-import time
-#所需库 requests,Pywifi,comtypes,subprocess
+import sys
+import os
+
+try:
+    import netifaces
+    import socket
+    from subprocess import check_output
+    import requests
+    import subprocess
+    import time
+except ImportError:
+    try:
+        command_to_execute = "pip install netifaces socket subprocess request || easy_install netifaces socket subprocess request"
+        os.system(command_to_execute)
+    except OSError:
+        print("Can NOT install , Aborted!")
+        sys.exit(1)
 
 
-def get_ip_address():#获取当前ip
+# 查询网络状态
+def is_network_connected():
+    try:
+        html = requests.get("http://www.baidu.com", timeout=2)
+        print("网络正常")
+        return 1
+    except:
+        print("网络异常")
+        return 0
+
+# 获取当前ip
+def get_ip_address():
     hostname = socket.gethostname()
     return socket.gethostbyname(hostname)
 
+# 获取mac
+def get_mac_address():
+    import netifaces
+    routingNicName = netifaces.gateways()['default'][netifaces.AF_INET][1]
+    for interface in netifaces.interfaces():
+        if interface == routingNicName:
+            routingNicMacAddr = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]['addr']
+    return routingNicMacAddr
+
+# 获取当前wifi的SSID
 def current_ssid():
+    file_path = 'showSSID.cmd'
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as f:
+            f.write(
+                '''@echo off\n\nset x=None\n\n@for /f "tokens=1,2,3" %%i in ('netsh WLAN show interfaces') do (\n\nif [%%i]==[SSID] set x=%%k\n\n)\necho %x%''')
+        f.close()
     scanoutput = check_output([r"showssid.cmd"])  # 最好使用完整路径
     x = scanoutput.decode()
-    currentSSID = x[:-2]  #如果一直failed ,可以试一下此函数的结果，修改此处的范围
+    currentSSID = x[:-2]
     return currentSSID
 
-def connect_to_wifi(ssid):
-    wifi = pywifi.PyWiFi()  # 创建一个Wi-Fi对象
-    ifaces = wifi.interfaces()  # 获取所有的Wi-Fi接口
-    if len(ifaces) == 0:
-        print("No Wi-Fi interface found.")
-        return
-
-    iface = ifaces[0]  # 使用第一个Wi-Fi接口
-    iface.disconnect()  # 断开当前连接（如果有）
-
-    profile = pywifi.Profile()  # 创建一个Wi-Fi配置文件
-    profile.ssid = ssid  # 设置Wi-Fi配置文件的SSID
-    profile.auth = const.AUTH_ALG_OPEN  # 设置认证算法为开放系统
-    profile.akm.append(const.AKM_TYPE_NONE)  # 设置AKM类型为无密码
-    profile.cipher = const.CIPHER_TYPE_NONE  # 设置加密类型为无密码
-
-    iface.remove_all_network_profiles()  # 移除所有的网络配置文件
-    tmp_profile = iface.add_network_profile(profile)  # 添加新的网络配置文件
-
-    iface.connect(tmp_profile)  # 连接到指定的公共Wi-Fi网络
-    #iface.disconnect()  # 断开连接（这是为了确保新的配置生效）
-    iface.connect(tmp_profile)  # 重新连接到指定的公共Wi-Fi网络
-
-    print("Connecting to", ssid)
-    # 检查已连接的Wi-Fi网络
-    time.sleep(2)
-    connected_ssid = current_ssid()
-    for i in range(4):
-        if connected_ssid == ssid:
-            print("Successfully connected to", ssid)
-            break
-        else:
-            connected_ssid = current_ssid()#这里是防止系统反应过慢进行了多次获取，如果不放心可以也进行多次连接（将下面一行代码取消注释
-            #iface.connect(tmp_profile)  # 重新连接到指定的公共Wi-Fi网络
-    else:
-        print("Failed to connect to", ssid)
-
-def is_network_connected():
-    try:
-        socket.create_connection(("www.baidu.com", 80))#测试网络是否连接
+# 判断是否连接到待登陆网络
+def is_dst_connected(ssid):
+    if ssid == current_ssid():
         return True
-    except OSError:
+    else:
         return False
 
+# 获取账户信息
+def account_get():
+    file_path = 'account.txt'
+    if not os.path.exists(file_path):
+        print('请输入账号：\n')
+        with open(file_path, 'w') as f:
+            account = input('账号：')
+            password = input('密码：')
+            f.write(account + '\n' + password)
+    else:
+        with open(file_path, 'r') as f:
+            account = f.readline()[:-1]
+            password = f.readline()
+    f.close()
+    return account, password
+
+# 认证登陆
 def auth():
-    # 认证登陆
     ip_address = get_ip_address()
-    url = 'http://172.16.253.3:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=USERNAME&user_password=PASSWORD&wlan_user_ip=%s&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3.2&v=6962' % ip_address  # 这行是你需要根据自己的情况修改的地方
-    #url中的username和password都是你的用户名和密码！！需更换！
-    response = requests.get(url).status_code  # 直接利用 GET 方式请求这个 URL 同时获取状态码
+    mac_address = get_mac_address()
+    account, password = account_get()
+    url0 = 'http://172.16.253.3:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%s&user_password=%s&wlan_user_ip=%s&wlan_user_ipv6=&wlan_user_mac=&wlan_ac_ip=&wlan_ac_name=' % (
+    account, password, ip_address)
+    url1 = 'http://172.16.253.3:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=8&user_account=%s&user_password=%s&wlan_user_ip=%s&wlan_user_ipv6=&wlan_user_mac=%s&wlan_ac_ip=172.20.0.165&wlan_ac_name=' % (
+    account, password, ip_address, mac_address)
+    url2 = 'http://172.16.253.3:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=8&user_account=%s&user_password=%s&wlan_user_ip=%s&wlan_user_ipv6=&wlan_user_mac=%s&wlan_ac_ip=172.20.0.165&wlan_ac_name=' % (
+    account, password, ip_address, mac_address)
+    response = [requests.get(url0).status_code, requests.get(url1).status_code,
+                requests.get(url2).status_code]  # 直接利用 GET 方式请求这个 URL 同时获取状态码
     print("状态码{}".format(response))  # 打印状态码
     if is_network_connected():
         print("登陆成功！")
+        return True
     else:
         print("登陆失败！")
+        return False
+
+# 连接wifi
+def connect_to_wifi(ssid):
+    name_of_router = ssid
+    print("Connecting to", ssid)
+    os.system('chcp 65001')
+    os.system(f'''cmd /c "netsh wlan connect name={name_of_router}"''')
+    # 检查已连接的Wi-Fi网络
+    time.sleep(2)
+    for i in range(4):
+        if is_dst_connected(ssid):
+            print("Successfully connected to", ssid)
+            break
+    else:
+        print("Failed to connect to", ssid)
 
 
 if __name__ == '__main__':
     ssid = "ahu.portal"  # 要连接的公共 Wi-Fi 网络的 SSID
-    currentssid = current_ssid()  # 获取当前连接的公共网络的SSID
-    #判断是否连接到网络
-    if currentssid == ssid:
-        print("连接到ahu.portal！将进行认证！")
+    if is_network_connected() and is_dst_connected(ssid):
+        print("ahu.portal已连接！")
     else:
-        if is_network_connected():
-            print("网络已经连接！将切换至ahu.portal,此过程可能会失败，若无响应请重试！")
-        else:
-            print("当前网络未连接，将要连接ahu.portal！")
+        print("shu.portal未正确连接！将进行连接和让认证！")
         connect_to_wifi(ssid)
-    if is_network_connected():
-        pass
-    else:
-        # 认证登陆，如果只需要认证登陆功能则将main中的其余代码注释
         auth()
-
-
-
-
-
+    auth() # 保险
--- auth.py
+++ auth.py
@@ -1,28 +1,60 @@
-import socket
-import requests
+import sys
+import os
+try:
+    import netifaces
+    import socket
+    from subprocess import check_output
+    import requests
+    import subprocess
+    import time
+except ImportError:
+    try:
+        command_to_execute = "pip install netifaces socket subprocess request || easy_install netifaces socket subprocess request"
+        os.system(command_to_execute)
+    except OSError:
+        print ("Can NOT install , Aborted!")
+        sys.exit(1)
 
-def get_ip_address():#获取当前ip
+# 获取当前ip
+def get_ip_address():
     hostname = socket.gethostname()
     return socket.gethostbyname(hostname)
 
-def is_network_connected():
-    try:
-        socket.create_connection(("www.baidu.com", 80))#测试网络是否连接
-        return True
-    except OSError:
-        return False
+# 获取mac
+def get_mac_address():
+    import  netifaces
+    routingNicName = netifaces.gateways()['default'][netifaces.AF_INET][1]
+    for interface in netifaces.interfaces():
+        if interface == routingNicName:
+            routingNicMacAddr = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]['addr']
+    return routingNicMacAddr
 
-def auth():
-    # 认证登陆
-    ip_address = get_ip_address()
-    url = 'http://172.16.253.3:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=USERNAME&user_password=PASSWORD&wlan_user_ip=%s&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3.2&v=6962' % ip_address  # 这行是你需要根据自己的情况修改的地方
-    #url中的username和password都是你的用户名和密码！！需更换！
-    response = requests.get(url).status_code  # 直接利用 GET 方式请求这个 URL 同时获取状态码
-    print("状态码{}".format(response))  # 打印状态码
-    if is_network_connected():
-        print("登陆成功！")
+# 获取账户信息
+def account_get():
+    file_path='account.txt'
+    if not os.path.exists(file_path):
+        print('请输入账号：\n')
+        with open(file_path,'w') as f:
+            account=input('账号：')
+            password = input('密码：')
+            f.write(account+'\n'+password)
     else:
-        print("登陆失败！")
+        with open(file_path,'r') as f:
+            account = f.readline()[:-1]
+            password = f.readline()
+    f.close()
+    return account,password
+
+# 认证登陆
+def auth():
+    ip_address = get_ip_address()
+    mac_address = get_mac_address()
+    account,password = account_get()
+    url0 = 'http://172.16.253.3:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%s&user_password=%s&wlan_user_ip=%s&wlan_user_ipv6=&wlan_user_mac=&wlan_ac_ip=&wlan_ac_name=' % (account, password, ip_address)
+    url1 = 'http://172.16.253.3:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=8&user_account=%s&user_password=%s&wlan_user_ip=%s&wlan_user_ipv6=&wlan_user_mac=%s&wlan_ac_ip=172.20.0.165&wlan_ac_name=' % (account, password, ip_address, mac_address)
+    url2 = 'http://172.16.253.3:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=8&user_account=%s&user_password=%s&wlan_user_ip=%s&wlan_user_ipv6=&wlan_user_mac=%s&wlan_ac_ip=172.20.0.165&wlan_ac_name=' % (account, password, ip_address, mac_address)
+    response = [requests.get(url0).status_code, requests.get(url1).status_code, requests.get(url2).status_code]  # 直接利用 GET 方式请求这个 URL 同时获取状态码
+    print("状态码{}".format(response))  # 打印状态码
 
 if __name__ == '__main__':
     auth()
